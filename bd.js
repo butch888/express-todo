430f9e5d337d075325ea0a8563b707b611007123
butch888 <never_man@mail.ru>
2024-08-01 14:35:34 +0300

    add connect

--- bd.js
+++ bd.js
@@ -9,3 +9,15 @@ const pool = new Pool({
 });
 
 module.exports = pool;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  user: 'gen_user',
+  host: '147.45.250.75',
+  database: 'default_db',
+  password: 'B:hMvzo=2Ad&$H',
+  port: 5432,
+});
+
+client.connect();
