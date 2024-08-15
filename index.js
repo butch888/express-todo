aaad6fa20e743e68ee7e3f8882d41de1cd30e119
butch888 <never_man@mail.ru>
2024-08-15 17:47:45 +0300

    Chenge server

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-const express = require('express');
-require('dotenv').config()
-const app = express();
-const pool = require('./bd');
-const cors = require('cors');
+// const express = require('express');
+// require('dotenv').config()
+// const app = express();
+// const pool = require('./bd');
+// const cors = require('cors');
 
-const getTasksRouter = require('./rest-api/getTasks');
-const addTasksRouter = require('./rest-api/addTask');
-const editTaskRouter = require('./rest-api/editTask');
-const isdoneTaskRouter = require('./rest-api/isdoneTask');
-const delTaskRouter = require('./rest-api/delTask');
+// const getTasksRouter = require('./rest-api/getTasks');
+// const addTasksRouter = require('./rest-api/addTask');
+// const editTaskRouter = require('./rest-api/editTask');
+// const isdoneTaskRouter = require('./rest-api/isdoneTask');
+// const delTaskRouter = require('./rest-api/delTask');
 
-app.use(cors());
+// app.use(cors());
 
-// получить все записи по id
-app.use('/', getTasksRouter);
+// // получить все записи по id
+// app.use('/', getTasksRouter);
 
-// добавить задачу
-app.use('/', addTasksRouter);
+// // добавить задачу
+// app.use('/', addTasksRouter);
 
-// редактировать текст задачи по id
-app.use('/', editTaskRouter);
+// // редактировать текст задачи по id
+// app.use('/', editTaskRouter);
 
-// отметить задачу выполненной по id
-app.use('/', isdoneTaskRouter);
+// // отметить задачу выполненной по id
+// app.use('/', isdoneTaskRouter);
 
-// удалить задачу по id
-app.use('/', delTaskRouter);
+// // удалить задачу по id
+// app.use('/', delTaskRouter);
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server has started on PORT: ${PORT}`)
-});
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`Server has started on PORT: ${PORT}`)
+// });
+
+
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
